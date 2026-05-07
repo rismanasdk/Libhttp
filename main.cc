cdcaf6ba44b21721f21cf088cb4165ff7989e839
rismanasdk <rismanh158@gmail.com>
2026-05-07 14:09:34 +0700

    feat: set basic auth

--- main.cc
+++ main.cc
@@ -6,10 +6,13 @@ int main() {
     http::Session session("http://example.com");
     session.set_header("User-Agent", "requests-like-demo/1.0");
     session.set_cookie("visitor", "true");
+    session.set_basic_auth("demo", "demo");
 
     http::RequestOptions options;
     options.params["page"] = "1";
     options.allow_redirects = true;
+    options.retry_count = 1;
+    options.retry_delay_ms = 100;
 
     try {
         http::Response response = session.get("/", options);
