76c9b794ba0fceac7e656d19de06e32c08492abc
rismanasdk <rismanh158@gmail.com>
2026-05-08 10:10:59 +0700

    build: update

--- portfile.cmake
+++ portfile.cmake
@@ -8,6 +8,12 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(PACKAGE_NAME libhttp CONFIG_PATH lib/cmake/libhttp)
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/lib"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
