87c079a0cc73ea0d842b596d8e2943e1f4fe7ac9
rismanasdk <rismanh158@gmail.com>
2026-05-08 08:43:26 +0700

    build: add vcpkg release checksum

--- portfile.cmake
+++ portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rismanasdk/libhttp
     REF v2.0.0
-    SHA512 0
+    SHA512 d5fc515750a1a403446d7ca39c06eaae3b604e4465886186a9673270612f6fa692e332a0218d0fb9846fa2a7195e4c8c6549716ccfb41690c130b8a5bab33b80
     HEAD_REF main
 )
 
